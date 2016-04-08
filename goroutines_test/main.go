c925e7e953ac4496d400cd222aa71db58940b5a3
Haiyan Meng <hmeng@nd.edu>
2016-04-08 12:19:14 -0400

    Test anonymous function

--- goroutines_test/main.go
+++ goroutines_test/main.go
@@ -9,7 +9,7 @@ var a string
 var done chan int = make(chan int)
 
 func f() {
-	a = "hello world!"
+	a = "I am a normal function!\n\tI am using the global variable a!\n\thello world!"
 	done <- 1
 }
 
@@ -23,9 +23,13 @@ func main() {
 
 	// anonymous function can refer to variables from the enclosing function, `main` function here.
 	go func() {
-		b = "inner hello world!"
+		b = "I am an anonymous function defined in the main function!\n\tI am using the variable b defined in the main function!\n\tinner hello world!"
 		done1 <- 1
 	}()
 	<- done1
 	fmt.Println(b)
+
+	func() {
+		fmt.Println("I am just a tiny Anonymous Function (Function Literal)!")
+	} ()
 }
